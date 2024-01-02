9632ae7e54392519d056e11fd07ab36f5cdffa9a
Auyushg <115849469+Auyushg@users.noreply.github.com>
2024-01-02 17:41:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
   const toggleBtn = document.querySelector('.toggle-sidebar');
   const sidebar = document.getElementById('sidebar');
-  const contentContainer = document.querySelector('.content-container');
+  const mainContent = document.getElementById('mainContent');
 
   toggleBtn.addEventListener('click', function () {
     sidebar.classList.toggle('active');
-    contentContainer.classList.toggle('active');
+    mainContent.classList.toggle('active');
   });
 });
